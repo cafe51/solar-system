065284a07d12b120bd05fbd14831faf5f33cf872
cafe51 <cafecafe51@hotmail.com>
2022-04-13 20:00:54 -0400

    requisito 7 resolvido

---
+++ src/components/Missions.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Missions extends React.Component {
+  render() {
+    // const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="missions">
+        algo
+      </div>
+    );
+  }
+}
+
+// Missions.propTypes = {
+//   planetName: PropTypes.string.isRequired,
+//   planetImage: PropTypes.string.isRequired,
+// };
+
+export default Missions;
